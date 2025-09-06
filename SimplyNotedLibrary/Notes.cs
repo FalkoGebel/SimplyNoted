247da902131ed5dcf116b253c26c397e84e9bbb2
Falko Gebel <falko-gebel@live.de>
2025-09-06 14:46:25 +0200

    Worked on notes handling methods

---
+++ SimplyNotedLibrary/Notes.cs
@@ -0,0 +1,31 @@
+﻿using SimplyNotedLibrary.Models;
+
+namespace SimplyNotedLibrary
+{
+    public class Notes
+    {
+        public List<NoteModel> _notes = [];
+
+        public int AddNote()
+        {
+            var latestNote = _notes.OrderBy(n => n.Id).LastOrDefault();
+            int id = latestNote == null ? 1 : latestNote.Id + 1;
+
+            _notes.Add(new NoteModel(id));
+
+            return id;
+        }
+
+        public NoteModel GetNote(int id)
+            => _notes.Where(n => n.Id == id).FirstOrDefault()
+                ?? throw new ArgumentException($"Note with id \"{id}\" does not exist.");
+
+        public void UpdateNote(NoteModel note)
+        {
+            var existingNote = GetNote(note.Id);
+
+            _notes.Remove(existingNote);
+            _notes.Add(note);
+        }
+    }
+}
---
+++ SimplyNotedLibraryTests/NoteTests.cs
@@ -0,0 +1,111 @@
+﻿using FluentAssertions;
+using SimplyNotedLibrary;
+using SimplyNotedLibrary.Models;
+
+namespace SimplyNotedLibraryTests
+{
+    [TestClass]
+    public sealed class NoteTests
+    {
+        [TestMethod]
+        public void Create_new_note_the_first_time_and_return_1_as_id()
+        {
+            //Arrange
+            Notes Notes = new();
+
+            // Act
+            int id = Notes.AddNote();
+
+            // Assert
+            id.Should().Be(1);
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(53)]
+        [DataRow(99)]
+        public void Create_new_note_for_given_times_and_return_correct_id(int notesToAdd)
+        {
+            //Arrange
+            Notes Notes = new();
+            int lastId = 0;
+
+            // Act
+            for (int i = 0; i < notesToAdd; i++)
+                lastId = Notes.AddNote();
+
+            // Assert
+            lastId.Should().Be(notesToAdd);
+        }
+
+        [TestMethod]
+        public void Try_to_get_note_with_invalid_id_and_get_exception()
+        {
+            //Arrange
+            Notes Notes = new();
+
+            // Assert
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                NoteModel note = Notes.GetNote(1);
+            });
+        }
+
+        [TestMethod]
+        public void Create_new_note_and_get_it_and_correct_fields()
+        {
+            //Arrange
+            Notes Notes = new();
+            int id = Notes.AddNote();
+
+            // Act
+            NoteModel note = Notes.GetNote(id);
+
+            // Assert
+            note.Id.Should().Be(id);
+            note.Title.Should().Be(string.Empty);
+            note.Content.Should().Be(string.Empty);
+            note.CreatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(30));
+            note.UpdatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(30));
+        }
+
+        [TestMethod]
+        public void Try_to_update_not_existing_note_and_get_exception()
+        {
+            //Arrange
+            Notes Notes = new();
+            NoteModel noteToUpdate = new(1);
+
+            // Assert
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Notes.UpdateNote(noteToUpdate);
+            });
+        }
+
+        [TestMethod]
+        public void Update_note_and_get_it_and_correct_fields()
+        {
+            //Arrange
+            Notes Notes = new();
+            int id = Notes.AddNote();
+            NoteModel note = Notes.GetNote(id);
+            string title = "New Title";
+            string content = "New Content";
+
+            // Act
+            note.Title = title;
+            note.Content = content;
+            Notes.UpdateNote(note);
+            note = Notes.GetNote(note.Id);
+
+            // Assert
+            note.Id.Should().Be(id);
+            note.Title.Should().Be(title);
+            note.Content.Should().Be(content);
+            note.UpdatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(20));
+        }
+
+        // TODO - add tests for deleting note
+    }
+}
