7ebda5f090d0c6666dcb8209b97e36ac096578c9
Falko Gebel <falko-gebel@live.de>
2025-09-13 22:56:22 +0200

    Added WPF UI and basic structure

---
+++ SimplyNotedUiWpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SimplyNotedLibrary;
+using SimplyNotedLibrary.Models;
+
+namespace SimplyNotedUiWpf.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private readonly Notes _notes = new();
+
+        public MainViewModel()
+        {
+        }
+
+        [ObservableProperty]
+        private List<NoteModel> _noteModels = [];
+    }
+}
