b8fb049357a9689ffecf39fc4523b1bce48ea128
Falko Gebel <falko-gebel@live.de>
2025-09-15 23:28:45 +0200

    New note function in WPF added, including file operations

--- SimplyNotedUiWpf/ViewModels/MainViewModel.cs
+++ SimplyNotedUiWpf/ViewModels/MainViewModel.cs
@@ -1,18 +1,40 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using SimplyNotedLibrary;
 using SimplyNotedLibrary.Models;
+using System.Windows;
 
 namespace SimplyNotedUiWpf.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
-        private readonly Notes _notes = new();
+        private readonly string _pathFile = Application.Current.Properties["PathFile"] as string
+                                                ?? throw new ArgumentNullException(nameof(_pathFile));
+        private Notes _notes = new();
 
         public MainViewModel()
         {
+            UpdateNotesFromFile();
         }
 
         [ObservableProperty]
         private List<NoteModel> _noteModels = [];
+
+        [RelayCommand]
+        private void NewNote()
+        {
+            int id = _notes.AddNote();
+            NoteModel newNote = _notes.GetNote(id);
+            newNote.Title = Properties.Literals.MainViewModel_NewNote_DefaultTitle;
+            _notes.UpdateNote(newNote);
+            _notes.SaveToFile(_pathFile);
+            UpdateNotesFromFile();
+        }
+
+        private void UpdateNotesFromFile()
+        {
+            _notes = Notes.LoadFromFile(_pathFile);
+            NoteModels = _notes.CurrentNotes;
+        }
     }
 }
