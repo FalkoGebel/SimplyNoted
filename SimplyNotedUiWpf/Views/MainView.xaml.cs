212bd8f6522935f4e089061ad015b70a79992440
Falko Gebel <falko-gebel@live.de>
2025-11-22 23:03:36 +0100

    Refactored MainView title functions to MVVM pattern

--- SimplyNotedUiWpf/Views/MainView.xaml.cs
+++ SimplyNotedUiWpf/Views/MainView.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Windows;
-using System.Windows.Input;
+﻿using SimplyNotedUiWpf.ViewModels;
+using System.Windows;
 
 namespace SimplyNotedUiWpf.Views
 {
@@ -8,40 +8,10 @@ namespace SimplyNotedUiWpf.Views
     /// </summary>
     public partial class MainView : Window
     {
-        public MainView()
+        public MainView(MainViewModel mainViewModel)
         {
-        }
-
-        private void ExitButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void HeaderLabel_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                DragMove();
-        }
-
-        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            BorderThickness = new Thickness(8);
-            WindowState = WindowState.Maximized;
-            MaximizeButton.Visibility = Visibility.Collapsed;
-            RestoreButton.Visibility = Visibility.Visible;
-        }
-
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void RestoreButton_Click(object sender, RoutedEventArgs e)
-        {
-            BorderThickness = new Thickness(0);
-            WindowState = WindowState.Normal;
-            RestoreButton.Visibility = Visibility.Collapsed;
-            MaximizeButton.Visibility = Visibility.Visible;
+            InitializeComponent();
+            DataContext = mainViewModel;
         }
     }
 }
