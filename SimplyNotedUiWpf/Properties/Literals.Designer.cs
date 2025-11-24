cde1aa3933a0c1b7cc57d3ded136455fdbf666fa
Falko Gebel <falko-gebel@live.de>
2025-11-24 23:30:10 +0100

    Designed and integrated own ConfirmView window

--- SimplyNotedUiWpf/Properties/Literals.Designer.cs
+++ SimplyNotedUiWpf/Properties/Literals.Designer.cs
@@ -19,7 +19,7 @@ namespace SimplyNotedUiWpf.Properties {
     // -Klasse über ein Tool wie ResGen oder Visual Studio automatisch generiert.
     // Um einen Member hinzuzufügen oder zu entfernen, bearbeiten Sie die .ResX-Datei und führen dann ResGen
     // mit der /str-Option erneut aus, oder Sie erstellen Ihr VS-Projekt neu.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Literals {
@@ -57,6 +57,15 @@ namespace SimplyNotedUiWpf.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Zeichenfolge, die Confirm ähnelt.
+        /// </summary>
+        public static string ConfirmView_Title {
+            get {
+                return ResourceManager.GetString("ConfirmView_Title", resourceCulture);
             }
         }
         
---
+++ SimplyNotedUiWpf/ViewModels/ConfirmViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SimplyNotedUiWpf.Views;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SimplyNotedUiWpf.ViewModels
+{
+    public partial class ConfirmViewModel : ObservableObject
+    {
+        private readonly ConfirmView _confirmView;
+
+        public bool Confirmed { get; private set; }
+
+        public ConfirmViewModel(string title, string message)
+        {
+            Message = message;
+            Title = title;
+
+            _confirmView = new(this);
+            _confirmView.ShowDialog();
+        }
+
+        [ObservableProperty]
+        private string _title;
+
+        [ObservableProperty]
+        private string _message;
+
+        [RelayCommand]
+        private void ConfirmViewMoveByHeaderLabelMouseDown(MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                _confirmView.DragMove();
+        }
+
+        [RelayCommand]
+        private void Yes(Window window)
+        {
+            Confirmed = true;
+            window.Close();
+        }
+
+        [RelayCommand]
+        private static void No(Window window)
+        {
+            window.Close();
+        }
+    }
+}
